5cfb5578a70dcddca31e612e2b7b144859e11901
MCE626 <Contact.MCE626@gmail.com>
2017-08-16 09:01:20 -0500

    swi++

--- commands/xendric/steamwarsimage.js
+++ commands/xendric/steamwarsimage.js
@@ -16,7 +16,7 @@ module.exports = class SWICommand extends commando.Command{
 
     async run(message){
         var body;
-        var roll = Math.floor(Math.random() * 12) + 1;
+        var roll = Math.floor(Math.random() * 15) + 1;
         if(roll == 4){
             body = 'http://xendric.net/assets/sw/' + roll + '.gif';
         } else {
